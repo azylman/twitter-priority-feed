03ecf0ff7f8ab2aa35591132eea12a611aa5347a
Alex Zylman <alex.zylman@gmail.com>
2011-12-15 05:33:24 -0600

    add an appropriate exception to table

--- src/com/zylman/twitter/Table.java
+++ src/com/zylman/twitter/Table.java
@@ -1,8 +1,9 @@
 package com.zylman.twitter;
 
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public interface Table {
-	void create(Statement s);
+	void create(Statement s) throws SQLException;
 	String getName();
 }
